11a00c1925feece12a092e541d628ce9843c8f9c
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2022-09-28 22:24:07 -0300

    Formularios de login/cadastro instanciados e testado visualização no html

--- SiteComunidade/forms.py
+++ SiteComunidade/forms.py
@@ -2,14 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
     email = StringField('Digite o seu melhor e-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirmação de senha', validators=[DataRequired, EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criar_conta = SubmitField('Criar Conta')
 
+
 class FormLogin(FlaskForm):
-    email = StringField('Digite seu e-mail cadastrado', validators=[DataRequired, Email()])
-    senha = PasswordField('Senha', validators=[DataRequired, Length(6, 20)])
+    email = StringField('Digite seu e-mail cadastrado', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     botao_submit_login = SubmitField('Fazer Login')
