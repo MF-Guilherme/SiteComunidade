36258a08ac54bfa97dcf55c1f27304aaf3b70d5c
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2022-11-15 23:15:30 -0300

    Bootstrap aplicado nos formulários e 'liberado' o método POST para eles

--- SiteComunidade/forms.py
+++ SiteComunidade/forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
-    email = StringField('Digite o seu melhor e-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criar_conta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('Digite seu e-mail cadastrado', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
