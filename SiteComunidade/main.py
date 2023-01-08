e8d74bd286b5529a5e5b65bc6df4981b14021722
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-01-08 20:23:54 -0300

    Instalando e configurando o DB SQLAlchemy

--- SiteComunidade/main.py
+++ SiteComunidade/main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 lista_usuarios = ['Lira', 'Joao', 'Alex', 'Alessandra']
 
 app.config['SECRET_KEY'] = 'fe7d7f6bde3b0ec4e090e64b6384caf1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
+database = SQLAlchemy(app)
 
 @app.route("/")
 def home():
