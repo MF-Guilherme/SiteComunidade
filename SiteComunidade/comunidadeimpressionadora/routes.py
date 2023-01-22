9403228bc21b6625ef2e6fd85cd82df7c041e733
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-01-22 19:22:48 -0300

    Login de Usuários com flask-login

--- SiteComunidade/comunidadeimpressionadora/routes.py
+++ SiteComunidade/comunidadeimpressionadora/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, flash, redirect, url_for
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuario
+from flask_login import login_user
 
 lista_usuarios = ['Lira', 'Joao', 'Alex', 'Alessandra']
 
@@ -27,10 +28,16 @@ def login():
     form_criarconta = FormCriarConta()
     # se o formulario foi validado ao submeter E o botão for o 'botao de login' (utilizando o AND pq temos 2 forms na mesma página
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        # exibir mensagem de login bem sucedido
-        flash(f'login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        # redirecionar para a homepage
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            # exibir mensagem de login bem sucedido
+            flash(f'login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            # redirecionar para a homepage
+            return redirect(url_for('home'))
+        else:
+            flash(f'Falha no login. E-mail ou Senha incorretos.', 'alert-danger')
+
     if form_criarconta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
         # instanciar o Usuario
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
