718092da7587ec74d4ea27e43d4915ba0e2bc4c5
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-02-20 17:05:43 -0300

    Método de contar os posts criado

--- SiteComunidade/comunidadeimpressionadora/models.py
+++ SiteComunidade/comunidadeimpressionadora/models.py
@@ -16,6 +16,9 @@ class Usuario(database.Model, UserMixin):
     posts = database.relationship('Post', backref='autor', lazy=True) # aqui criamos a relação do usuario com o post
     cursos = database.Column(database.String, nullable=False, default='Não informado')
 
+    def contar_posts(self):
+        return len(self.posts)
+
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
